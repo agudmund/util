214671c43c96af9afdfc0947ea3fb8b97dcc5a38
Aevar Gudmundsson <aevar.gudmundsson@gmail.com>
2017-01-26 09:14:10 +0100

    Added a target directory choice

--- testfile_gen.py
+++ testfile_gen.py
@@ -4,17 +4,22 @@
 # Usage: ./testfile_gen.py
 
 import os
+import argparse
 
-if not os.path.exists("testfiles"):
-	os.mkdir("testfiles")
+parser = argparse.ArgumentParser(description='Test file generator')
+parser.add_argument('-p','--path', type=str,help='Target path to place the files in',action="store",required=True)
+args = parser.parse_args()
+
+if not os.path.exists(args.path):
+	os.mkdir(args.path)
 
 sizes = [ 1, 5, 10, 50, 100 ]
 units = [ 'k', 'm', 'g' ]
 
 for unit in units:
 	for size in sizes:
-		data = {'current':''.join([str(size),unit]).zfill(4)}
+		data = {'current':''.join([str(size),unit]).zfill(4),'path':args.path}
 		print "--[ Creating Test file: %(current)s" % data
-		os.system( "mkfile -n %(current)s testfiles/%(current)s.testfile" % data )
+		os.system( "mkfile -n %(current)s %(path)s/%(current)s.testfile" % data )
 
 print 'All done, files are in a subdirectory called "testfiles"'
