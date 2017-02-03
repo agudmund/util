027c7c635f361a2aa4702ccdbddb34c3fa4081f1
Aevar Gudmundsson <aevar.gudmundsson@gmail.com>
2017-02-03 01:13:11 +0100

    Added human to bytesize conversion

--- lib/fileutil.py
+++ lib/fileutil.py
@@ -14,3 +14,16 @@ def size(num):
 	num = round(num,2)
 
 	return str(num),unit
+
+def bytesize(num):
+	'''Converts human readable file sizes into actual size'''
+
+	if not num[-1].isalpha():
+		print 'Please use 30G format, number ending in a letter'
+		return False
+	if num[-1] in ['k','K']:
+		return int(num[:-1]) * 1024
+	elif num[-1] in ['m','M']:
+		return (int(num[:-1]) * 1024) * 1024
+	elif num[-1] in ['g','G']:
+		return ((int(num[:-1]) * 1024) * 1024) * 1024
