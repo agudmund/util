8618072b13bab9b20a448c274f0731f9bef98d9c
Aevar Gudmundsson <aevar.gudmundsson@gmail.com>
2017-02-23 10:54:58 +0100

    slight cleanup of inspector snippet

--- inspector.py
+++ inspector.py
@@ -1,5 +1,11 @@
 import sys
 import inspect
-curframe = inspect.currentframe()
-calframe = inspect.getouterframes(curframe, 2)
-print >> sys.__stdout__,'setTrgPath----->caller name:', calframe[1][3]
+
+# Usage: {within a python script}
+# import inspector
+# inspector.listen() {will print to stdout all occurrences of said action}
+
+def listen():
+	curframe = inspect.currentframe()
+	calframe = inspect.getouterframes(curframe, 2)
+	print >> sys.__stdout__,'caller name:', calframe[1][1], calframe[1][3]
