2da84838184b8993b064fbd77e4bdb85ac6112cc
Aevar Gudmundsson <aevar.gudmundsson@gmail.com>
2017-03-02 10:27:15 +0100

    Extended arguments for icon assignment

--- icon_assign.py
+++ icon_assign.py
@@ -3,7 +3,14 @@
 # Usage: ./icon.py file icon.png
 
 import Cocoa
-import sys
+import argparse
 
-space = Cocoa.NSWorkspace.sharedWorkspace()
-space.setIcon_forFile_options_(Cocoa.NSImage.alloc().initWithContentsOfFile_(sys.argv[1]), sys.argv[2],0)
+def assign(file,icon):
+	space = Cocoa.NSWorkspace.sharedWorkspace()
+	space.setIcon_forFile_options_(Cocoa.NSImage.alloc().initWithContentsOfFile_(file), icon,0)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Batch icon assign for Mac OS')
+	parser.add_argument('-f','--file', help='File to treat',action="store")
+	parser.add_argument('-i','--icon', help='Icon to assign',action="store")
+	args = parser.parse_args()
