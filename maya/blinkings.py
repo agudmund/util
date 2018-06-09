93e6e0b5bc23771b7a6d9d7b65238feb934c37f9
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2018-06-09 06:57:12 +0000

    blinkings util

---
+++ maya/blinkings.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env mayapy
+# *-* coding:utf-8 *-*
+#
+#    Applies blink to a 3D Rig
+#   Timing is based off natural blinking patterns of a human
+#     ranges 65-85 frames at 24 fps
+#
+#  Usage:
+#     import blinkings as b
+#     b.apply_blink()
+#
+#   -aeVar
+
+import random
+import maya.cmds as m
+from random import randint
+from maya.cmds import playbackOptions
+
+def pick_frame(slider=True):
+	'''Returns frames based on time slider or full range for character blinking
+		-based on 65-85 frame intervales which is the human average blinking rate
+			: slider = Bases the range off the Timeslider, false means it will apply it to the full animation range
+	'''
+
+	blink_frames = []
+	rate = random.randint( 65, 85 )
+
+	count = 0
+	if slider:
+		start = int( playbackOptions( min=True, q=True ))
+		end = int( playbackOptions( max=True, q=True ))
+	else:
+		start = int( playbackOptions( ast=True, q=True ))
+		end = int( playbackOptions( aet=True, q=True ))
+
+	for i in xrange( start, end ):
+		count += 1
+		if count == rate:
+			blink_frames.append( i )
+			rate = randint( 65, 85 )
+			count = 0
+
+	return blink_frames
+
+def blink( frame, offset=0 ):
+	'''
+	Applies a blink to the eye controllers of a rig
+	  : frame = frame to start the blink on
+	  : offset = value offset if the controller is larger or smaller than normal
+	'''
+
+	seed = randint(1,5)
+
+	ctrl = 	{
+				'left': 'Normal_ctrlFK_Lf_Eye', # Left Eye Controller
+				'right': 'Normal_ctrlFK_Rt_Eye', # Right Eye Controller 
+				'up': 'upLidUD', # Upper Eyelid Attribute
+				'down': 'loLidUD' # Lower Eyelid Attribute
+			}
+	
+	m.setKeyframe( ctrl['left'], t=frame-seed, at=ctrl['up'], v=0 )
+	m.setKeyframe( ctrl['left'], t=frame-seed, at=ctrl['down'], v=0 )
+	m.setKeyframe( ctrl['right'], t=frame-seed, at=ctrl['up'], v=0 )
+	m.setKeyframe( ctrl['right'], t=frame-seed, at=ctrl['down'], v=0 )
+
+	m.setKeyframe( ctrl['left'], t=frame, at=ctrl['up'],v=4.65 + offset )
+	m.setKeyframe( ctrl['left'], t=frame, at=ctrl['down'],v=-3.37 + offset )
+	m.setKeyframe( ctrl['right'], t=frame, at=ctrl['up'],v=4.65 + offset )
+	m.setKeyframe( ctrl['right'], t=frame, at=ctrl['down'],v=-3.37 + offset )
+
+	m.setKeyframe( ctrl['left'], t=frame+seed, at=ctrl['up'], v=0 )
+	m.setKeyframe( ctrl['left'], t=frame+seed, at=ctrl['down'], v=0 )
+	m.setKeyframe( ctrl['right'], t=frame+seed, at=ctrl['up'], v=0 )
+	m.setKeyframe( ctrl['right'], t=frame+seed, at=ctrl['down'], v=0 )
+
+	return True
+
+def apply_blink():
+	'''
+	  Main procedure to run, collects frames and applies them to the rig
+	'''
+
+	frames = pick_frame()
+	for frame in frames:
+		blink( frame )
+
+	return True
+
+if __name__ == '__main__':
+	apply_blink()
