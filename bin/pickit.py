f7f30fb55829e6372f145c18d88f87e0f395ca70
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2019-02-27 13:43:27 +0000

    exclude picker

--- bin/pickit.py
+++ bin/pickit.py
@@ -3,4 +3,28 @@
 import os
 import random
 
-print (random.choice(os.listdir(os.getcwd())))
+
+exclude_path = os.path.join(os.getenv("TMP"),"pickit_exclude.tmp")
+working_path = os.getenv("project")
+
+if os.path.exists(exclude_path):
+	with open(exclude_path) as data:
+		rez = data.readlines()
+else:
+	rez = []
+
+exclude = [n.rstrip('\n') for n in rez]
+
+c = os.listdir(os.getenv("project"))
+candidates = [n for n in c if n not in exclude]
+candidate = random.choice(candidates)
+exclude.append(candidate)
+
+with open(exclude_path,'w') as data:
+	for n in exclude:
+		data.write('%s\n'%n)
+
+print(candidate)
+
+if len(c)==len(exclude):
+	os.remove(exclude_path)
