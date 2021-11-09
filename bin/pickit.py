25d5551c9f0db082d3794466db60b31b774f0baf
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2021-11-09 18:04:13 +0000

    sys.argv for arbitrary paths

--- bin/pickit.py
+++ bin/pickit.py
@@ -1,28 +1,40 @@
 #!C:\Program Files\Python36\python
 
 import os
+import sys
 import random
 
+# Add already selected ones to a list
 exclude_path = os.path.join(os.getenv("TMP"),"pickit_exclude.tmp")
-working_path = os.getenv("project")
-
 if os.path.exists(exclude_path):
 	with open(exclude_path) as data:
 		rez = data.readlines()
 else:
 	rez = []
 
-exclude = [n.rstrip('\n') for n in rez]
-c = os.listdir(os.getenv("project"))
-candidates = [n for n in c if n not in exclude]
-candidate = candidates[random.randrange(0,len(candidates))]
-exclude.append(candidate)
+def pickPath():
+	### Get Path from environment, if no commandline argument is supplied
+	if len(sys.argv)<2:
+		working_path = os.getenv("project")
+	else:
+		working_path = sys.argv[1]
 
-with open(exclude_path,'w') as data:
-	for n in exclude:
-		data.write('%s\n'%n)
+	return working_path
 
-print(candidate)
+if __name__ == '__main__':
 
-if len(c)==len(exclude):
-	os.remove(exclude_path)
+	working_path = pickPath()
+	exclude = [n.rstrip('\n') for n in rez]
+	c = os.listdir(working_path)
+	candidates = [n for n in c if n not in exclude]
+	candidate = candidates[random.randrange(0,len(candidates))]
+	exclude.append(candidate)
+
+	with open(exclude_path,'w') as data:
+		for n in exclude:
+			data.write('%s\n'%n)
+
+	print(candidate)
+
+	if len(c)==len(exclude):
+		os.remove(exclude_path)
