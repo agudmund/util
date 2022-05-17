d3f710550955a92e68346bc4e4372fddd21640a2
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2022-05-17 16:47:26 +0000

    Just a little resolution tweak of HD videos rather than quick scratches

--- bin/youtubeGrab.py
+++ bin/youtubeGrab.py
@@ -6,4 +6,12 @@ from pytube import YouTube
 url = r'%s'%sys.argv[-1]
 video  = YouTube( url )
 
-video.streams.filter(subtype='mp4').all()[0].download(output_path=r'C:\Users\normal\Downloads')
+
+rez = video.streams.filter(subtype='mp4')
+
+for n in rez:
+	if n.type == 'video':
+		if(len(n.resolution))>4:
+			itagz = n.itag
+			break
+video.streams.filter(subtype='mp4' ).get_by_itag(itagz).download(output_path=r'C:\Users\normal\Downloads')
